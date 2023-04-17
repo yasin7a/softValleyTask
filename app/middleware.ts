15e5d5b5058c850784ca3b49a1bb0dedcd1eede8
yasin7a <arafath7yasin@gmail.com>
2023-04-17 13:25:26 +0600

    completed

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,16 +1,33 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
+  const { pathname } = request.nextUrl;
+  if (pathname.startsWith("/_next")) return NextResponse.next();
   let cookie = request.cookies.get("auth");
 
-  if (request.nextUrl.pathname.startsWith("/login") && cookie) {
-    return NextResponse.rewrite(new URL("/", request.url));
+
+
+
+
+
+  if (pathname === "/login" && cookie) {
+    request.nextUrl.pathname = "/";
+    return NextResponse.redirect(request.nextUrl);
   }
 
-  if (request.nextUrl.pathname.startsWith("/") && !cookie) {
-    return NextResponse.rewrite(new URL("/login", request.url));
+
+
+  if (pathname === "/" && !cookie) {
+    request.nextUrl.pathname = "/login";
+    return NextResponse.redirect(request.nextUrl);
   }
+
+
+
+
+
+
+  return NextResponse.next();
 }
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.png).*)"],
