6ed400d78e3993844d4f5a4eacd9b118899f9e94
yasin7a <arafath7yasin@gmail.com>
2023-04-17 13:00:08 +0600

    completed

---
+++ app/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+let pathredirectAfterAuth = ["/login"];
+let pathredirectAfterAuthProtect = ["/"];
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const response = NextResponse.next();
+  let cookie = request.cookies.get("auth");
+
+  if (pathredirectAfterAuth.includes(pathname)) {
+    if (cookie) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+  if (pathredirectAfterAuthProtect.includes(pathname)) {
+    if (!cookie) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return response;
+}
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.png).*)"],
+};
